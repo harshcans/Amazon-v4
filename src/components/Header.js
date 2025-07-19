5b5c7969b0eda0fcb43477b747b6e62902db69e1
The HarshCans <harshsaifai@yandex.com>
2025-07-19 23:03:26 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import {
   SearchIcon,
   ShoppingCartIcon
 } from "@heroicons/react/outline";
-import { signIn, signOut, useSession } from "next-auth/client";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 import { selectItems } from "../slices/basketSlice";
